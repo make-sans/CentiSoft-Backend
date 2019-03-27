bc57d201c6ac9745fbf1ab0431e95f5c12226d07
Alexander Ignácz <ignaczalexander@gmail.com>
2019-03-27 13:15:34 +0100

    task get, post, delete

--- routes/task.js
+++ routes/task.js
@@ -1,23 +1,98 @@
 const express = require("express");
 const Task = require("../task/Task");
 const Project = require("../projects/Project");
+const Developer = require("../developers/Developer");
 const router = express.Router();
+
+//validation
+const validateTaskFields = require("../validation/task");
 
 //get all tasks for a project
 router.get("/:id/tasks", (req, res) => {
-  Task.find({ projectId: req.params.id })
-    .then(task => {
-      res.json(task);
+  Project.findById(req.params.id)
+    .then(project => {
+      Task.find({ projectId: project._id })
+        .then(task => {
+          res.json(task);
+        })
+        .catch(err => console.log(err));
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      res.status(404).json({ projectnotfound: "No project found with the id" });
+    });
 });
 
 //get a task for a project by id
 router.get("/:proj_id/tasks/:task_id", (req, res) => {
-  Task.find({ projectId: req.params.proj_id })
-    .then(tasks => {
-      res.json(tasks.filter(item => item._id === req.params.task_id));
+  Task.findById(req.params.task_id)
+    .then(task => {
+      if (task.projectId !== req.params.proj_id) {
+        return res
+          .status(404)
+          .json({ noprojectfound: "No project found with that id" });
+      }
+      res.json(task);
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      res.status(404).json({ notaskfound: "No task found with that id" });
+    });
+});
+
+//create a task
+router.post("/:proj_id/tasks", (req, res) => {
+  const { errors, isValid } = validateTaskFields(req.body);
+
+  //check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  Project.findById(req.params.proj_id)
+    .then(project => {
+      Developer.findById(req.body.developer)
+        .then(developer => {
+          const newTask = new Task({
+            name: req.body.name,
+            description: req.body.description,
+            created: req.body.created,
+            duration: req.body.duration,
+            projectId: project._id,
+            developerId: developer._id
+          });
+
+          newTask.save().then(task => res.json(task));
+        })
+        .catch(err => {
+          return res
+            .status(404)
+            .json({ developernotfound: "No developer found with that id" });
+        });
+    })
+    .catch(err => {
+      return res
+        .status(404)
+        .json({ projectnotfound: "No project found with that id", err });
+    });
+});
+//delete a task
+router.delete("/:proj_id/tasks/:task_id", (req, res) => {
+  Task.findById(req.params.task_id)
+    .then(task => {
+      if (task.projectId.toString() !== req.params.proj_id)
+        return res
+          .status(404)
+          .json({ msg: "Projectid does not equal params project id" });
+
+      task
+        .remove()
+        .then(() => res.json({ success: true }))
+        .catch(err => {
+          res.status(404).json({ msg: "Delete failed" });
+          console.log(err);
+        });
+    })
+    .catch(err => {
+      res.status(404).json({ tasknotfound: "Task not found with that id" });
+    });
 });
 module.exports = router;
---
+++ validation/task.js
@@ -0,0 +1,42 @@
+const isEmpty = require("./is-empty");
+const Validator = require("validator");
+const moment = require("moment");
+
+module.exports = function validateTaskFields(data) {
+  let errors = {};
+
+  data.name = !isEmpty(data.name) ? data.name : "";
+  data.description = !isEmpty(data.description) ? data.description : "";
+  data.created = !isEmpty(data.created) ? data.created : "";
+  data.duration = !isEmpty(data.duration) ? data.duration : "";
+  data.developer = !isEmpty(data.developer) ? data.developer : "";
+
+  if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
+    errors.name = "Name must be between 2 and 30 characters!";
+  }
+
+  if (Validator.isEmpty(data.name)) {
+    errors.name = "Name field is required";
+  }
+
+  if (Validator.isEmpty(data.description)) {
+    errors.description = "Description field is required";
+  }
+  if (!moment(data.created).isValid()) {
+    errors.created = "Invalid created date";
+  }
+  if (Validator.isEmpty(data.created)) {
+    errors.created = "Created field is required";
+  }
+  if (Validator.isEmpty(data.duration)) {
+    errors.duration = "Duration field is required";
+  }
+  if (Validator.isEmpty(data.developer)) {
+    errors.developer = "Developer field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
