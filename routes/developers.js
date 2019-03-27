23f69dcfa7492087400db3b0acb3042378410c2a
Nikola Anastasov Velichkov <nikola@velichkov-bg.com>
2019-03-27 10:10:16 +0100

    Created basic GET and POST handlers

--- routes/developers.js
+++ routes/developers.js
@@ -1,9 +1,32 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const Developer = require('../developers/Developer');
+
+/*
+ * /api/developers (get)
+ * /api/developers/{id} (get)
+ * /api/developers (post)
+ * /api/developers/{id} (put)
+ * /api/developers/{id} (delete)
+ */
+
+router.get('/', (req, res) => {
+    Developer.find()
+        .then((developers) => {
+            res.json(developers)
+        })
+})
+
+router.post('/', (req, res) => {
+    // TODO validate json body
+
+    const dev = new Developer({
+        name: req.body.name,
+        email: req.body.email
+    })
+
+    dev.save().then(dev => res.send(dev));
+})
 
 module.exports = router;
